41120f760a46a305f5e8ef13c178e499bb1dea78
anshikaahuja19 <ahujaanshika125@gmail.com>
2026-04-01 23:08:22 +0530

    changed api url

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ document.addEventListener("DOMContentLoaded", function() {
 let allQuotes = [];
 
 async function fetchQuote() {
-  const proxyUrl = 
-    "https://api.allorigins.win/get?url=" + ("https://zenquotes.io/api/quotes");
+  const Url = "https://dummyjson.com/quotes?limit=50";
 
-  const res  = await fetch(proxyUrl);
+  const res  = await fetch(Url);
   const data = await res.json();
-  allQuotes = JSON.parse(data.contents); // store all 50
+  allQuotes = data.quotes; // store all 50
   showQuote(allQuotes[0]);               // show the first one
 
   // Just log it first — see what you're working with
@@ -18,8 +17,8 @@ async function fetchQuote() {
   
 }
 function showQuote(quotes) {
-  document.getElementById("quote-text").textContent   = `"${quotes.q}"`;
-  document.getElementById("quote-author").textContent = `— ${quotes.a}`;
+  document.getElementById("quote-text").textContent   = `"${quotes.quote}"`;
+  document.getElementById("quote-author").textContent = `— ${quotes.author}`;
 }
 function refreshQuote() {
   const randomIdx = Math.floor(Math.random() * allQuotes.length);
